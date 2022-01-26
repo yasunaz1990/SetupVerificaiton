3d68a051357b3746b6d8675e610cc44123fd5716
Nijat Muhtar <nijatmn@gmail.com>
2022-01-26 17:47:58 -0500

    Project for intellij set-up verification

---
+++ src/VerifySetup.java
@@ -0,0 +1,9 @@
+public class VerifySetup {
+
+    public static void main(String[] args) {
+        System.out.println("====================================");
+        System.out.println("If you are seeing this message,");
+        System.out.println("you are all set!!");
+        System.out.println("====================================");
+    }
+}
